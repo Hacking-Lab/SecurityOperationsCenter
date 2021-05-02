0c1b6fc9ca5ec83c2d51d782df88202125ffef36
CumaNeoTerra <CumaNeoTerra@users.noreply.github.com>
2021-05-02 17:14:08 +0200

    Update import_cert.ps1

--- traefik/certs/import_cert.ps1
+++ traefik/certs/import_cert.ps1
@@ -3,3 +3,6 @@ $rootStore = Get-Item cert:\LocalMachine\Root
 $rootStore.Open("ReadWrite")
 $rootStore.Add($cert)
 $rootStore.Close()
+
+
+Import-Certificate -FilePath "ca.crt" -CertStoreLocation Cert:\LocalMachine\Root
